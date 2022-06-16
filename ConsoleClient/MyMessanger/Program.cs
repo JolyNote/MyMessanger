8ad6686046f008d3933c4d06bc48cac09bf9c46b
Роман Русанов <romdek174@gmail.com>
2022-06-17 02:11:23 +0500

    Version 0.0.1

---
+++ ConsoleClient/MyMessanger/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyMessanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+            Console.WriteLine(message.ToString());
+        }
+    }
+}
