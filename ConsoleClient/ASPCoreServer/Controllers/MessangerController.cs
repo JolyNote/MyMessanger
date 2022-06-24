82e100a237b36d852fa8e8640b9144c31b246234
Роман Русанов <romdek174@gmail.com>
2022-06-25 00:07:38 +0500

    Идет разработка сервера

---
+++ ConsoleClient/ASPCoreServer/Controllers/MessangerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPCoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessangerController : ControllerBase
+    {
+        
+        // GET api/<MessangerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MessangerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
